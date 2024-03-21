57e32b26668dfbb418d57f93f43e9df017353452
LorenzoFerFra <39970420+LorenzoFerFra@users.noreply.github.com>
2024-03-20 22:03:24 -0300

    Primer commit
    primer comit otra vez, borrando html placeholder y creando componente lorem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoremComponent } from './lorem/lorem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoremComponent
   ],
   imports: [
     BrowserModule,
